d7526e09a947bc161a8a65f3899e5d6fa8eb1e66
Cyrille DUTOIT <cyrille.dutoit@gmail.com>
2022-05-10 10:04:43 +0200

    Mise en place email-validator

--- back/routes/user.js
+++ back/routes/user.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const userCtrl = require("../controllers/user");
+
+const validEmail =require("../middleware/email")
 const passWord = require("../middleware/password");
 
-router.post("/signup", passWord, userCtrl.signup);
+router.post("/signup", validEmail, passWord, userCtrl.signup);
 router.post("/login", userCtrl.login);
 
 module.exports = router;
