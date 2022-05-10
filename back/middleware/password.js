27055b21cbfd6c124dd4f46e6ebe4af06caad3de
Cyrille DUTOIT <cyrille.dutoit@gmail.com>
2022-05-10 09:48:16 +0200

    Amélioration PassWord-Validator

--- back/middleware/password.js
+++ back/middleware/password.js
@@ -1,15 +1,4 @@
-const PasswordValidator = require("password-validator");
-
-const pwdSchema = new PasswordValidator();
-
-pwdSchema
-  .is().min(8) // Minimum length 8
-  .is().max(20) // Maximum length 100
-  .has().uppercase() // Must have uppercase letters
-  .has().lowercase() // Must have lowercase letters
-  .has().digits() // Must have at least 2 digits
-  .has().not().spaces() // Should not have spaces
-  .is().not().oneOf(["Passw0rd", "Password123"]); // Blacklist these values
+const pwdSchema = require("../models/password");
 
 module.exports = (req, res, next) => {
   if (pwdSchema.validate(req.body.password)) {
